967eaedcc1b2f8e4c02a90018e19df6524eb3b7e
bug-hunter333 <miyulasinduwara2@gmail.com>
2024-09-12 13:24:04 +0530

    medi.js

--- medi.js
+++ medi.js
@@ -1,6 +1,7 @@
 let cart = [];
 let totalPrice = 0;
 
+// Function to add an item to the cart
 function addToCart(medicineName, quantity, price) {
     let cartItem = {
         name: medicineName,
@@ -11,6 +12,7 @@ function addToCart(medicineName, quantity, price) {
     updateCartTable();
 }
 
+// Function to update the cart table in the HTML
 function updateCartTable() {
     const cartItemsElement = document.getElementById('cart-items');
     cartItemsElement.innerHTML = '';
@@ -28,4 +30,26 @@ function updateCartTable() {
     });
 
     document.getElementById('total-price').innerText = totalPrice.toFixed(2);
+}
+
+// Function to save current cart items to favourites (local storage)
+function addToFavourites() {
+    if (cart.length > 0) {
+        localStorage.setItem('favourites', JSON.stringify(cart));
+        alert('Favourites saved successfully!');
+    } else {
+        alert('No items in the cart to save as favourites.');
+    }
+}
+
+// Function to load saved favourites and add them to the cart
+function applyFavourites() {
+    const savedFavourites = localStorage.getItem('favourites');
+    if (savedFavourites) {
+        cart = JSON.parse(savedFavourites);
+        updateCartTable();
+        alert('Favourites applied to your cart!');
+    } else {
+        alert('No favourites found.');
+    }
 }
